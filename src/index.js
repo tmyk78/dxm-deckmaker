c1d335811a068f9b1b9ecfd8a407b290e232ae2a
akit0 <akimototomoyuki@gmail.com>
2018-03-22 00:10:23 +0900

    [add]スタイルを整形

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-import { functionAllCards, removeAllCards, cloneCarda } from './components/functionAllCards.js'
+import { functionAllCards, removeAllCards, cloneCarda } from './js/functionAllCards.js'
 
-import cardList from './components/cardList.js'
+import './css/style.css'
+
+import cardList from './js/cardList.js'
 
 class App extends Component {
 
@@ -20,43 +22,50 @@ class App extends Component {
     })
   }
 
-  onClick() {
+  onClickAllianceSelectButton() {
     const cardPool = document.querySelector('.cardPool')
 
     functionAllCards(removeAllCards)
 
     cardList[this.state.alliance].forEach(item => {
       cardPool.insertAdjacentHTML('beforeend',
-      `<p class="card js-card"><img src="${item.path}" alt="${item.name}" width="100" height="auto"></p>`
-    )
-  }, false)
+      `<div class="card js-card"><img src="${item.path}" alt="${item.name}" class="card__image"></div>`
+      )
+    }, false)
 
     functionAllCards(cloneCarda)
   }
 
   render() {
     return (
-      <div>
-      <h1>DXM DeckMaker</h1>
-    <select onChange={(event) => this.onChangeAlliance(event)}>
-  <option value="olympus">オリンポス</option>
-      <option value="trinity">トリニティ</option>
-      </select>
-      <br />
-      <button onClick={() => this.onClick()}>セレクト！</button>
-    <div className="cardPool">
-      <h2>Card Pool</h2>
-    </div>
-    <div className="deckPool">
-      <h2>Deck Pool</h2>
-    </div>
-    </div>
-  )
+      <div className="wrapper">
+        <header className="header">
+          <h1 className="title">DXM DeckMaker</h1>
+          <div class="selectBox">
+            <select className="selectBox__select" onChange={(event) => this.onChangeAlliance(event)}>
+              <option value="olympus">オリンポス</option>
+              <option value="trinity">トリニティ</option>
+            </select>
+          </div>
+          <button className="button" onClick={() => this.onClickAllianceSelectButton()}>セレクト</button>
+        </header>
+        <div className="content">
+          <div className="cardList">
+            <h2 className="cardList__title">Card Pool</h2>
+            <div className="cardList__body cardPool"></div>
+          </div>
+          <div className="cardList">
+            <h2 className="cardList__title">Deck Pool</h2>
+            <div className="cardList__body deckPool"></div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
 ReactDOM.render(
-<App />,
+  <App />,
   document.getElementById('root')
 )
 
---
+++ src/js/cardList.js
@@ -0,0 +1,64 @@
+const cardList = {
+  olympus: [
+    // mephisto
+    {
+      path: 'img/olympus/mephisto_olympus_000001.png',
+      name: 'アテナイの綱渡り師',
+    },
+    {
+      path: 'img/olympus/mephisto_olympus_000002.png',
+      name: 'アテナイの喜劇役者',
+    },
+    {
+      path: 'img/olympus/mephisto_olympus_000003.png',
+      name: 'コリントスのワイン売り',
+    },
+
+    // mexica
+    {
+      path: 'img/olympus/mexica_olympus_000001.png',
+      name: 'アテナイの操舵手',
+    },
+    {
+      path: 'img/olympus/mexica_olympus_000002.png',
+      name: 'アンピトリテのイルカ',
+    },
+    {
+      path: 'img/olympus/mexica_olympus_000003.png',
+      name: '狩聖 アタランテ',
+    }
+  ],
+
+  trinity: [
+    // mephisto
+    {
+      path: 'img/trinity/mephisto_trinity_000001.png',
+      name: 'ヴェネツィアの囚人',
+    },
+    {
+      path: 'img/trinity/mephisto_trinity_000002.png',
+      name: 'ジェノヴァの菓子売り',
+    },
+    {
+      path: 'img/trinity/mephisto_trinity_000003.png',
+      name: 'ジェノヴァの奇術師',
+    },
+
+    // mexica
+    {
+      path: 'img/trinity/mexica_trinity_000001.png',
+      name: '聖堂領の放浪学僧',
+    },
+    {
+      path: 'img/trinity/mexica_trinity_000002.png',
+      name: 'ジェノヴァの花売り',
+    },
+    {
+      path: 'img/trinity/mexica_trinity_000003.png',
+      name: 'グリフィンの荷持',
+    }
+  ]
+
+}
+
+export default cardList
